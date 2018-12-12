e1dd4237f08bbfdee5ac1888650d2e111429a529
Ravali <rk694@njit.edu>
2018-12-12 11:36:58 -0800

    Task: Installed Dusk and Browser folder got created.

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -5,7 +5,7 @@ namespace PHPSTORM_META {
 
    /**
     * PhpStorm Meta file, to provide autocomplete information for PhpStorm
-    * Generated on 2018-11-18 20:21:59.
+    * Generated on 2018-12-11 23:30:54.
     *
     * @author Barry vd. Heuvel <barryvdh@gmail.com>
     * @see https://github.com/barryvdh/laravel-ide-helper
