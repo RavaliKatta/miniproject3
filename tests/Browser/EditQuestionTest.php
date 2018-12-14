60a53e03efbeba66b55c9ed049e0e9eaff2c1eda
Ravali <rk694@njit.edu>
2018-12-14 10:16:50 -0800

    Task: Performed dusk test to edit question.

---
+++ tests/Browser/EditQuestionTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Tests\Browser;
+
+use Tests\DuskTestCase;
+use Laravel\Dusk\Browser;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+
+class EditQuestionTest extends DuskTestCase
+{
+    /**
+     * A Dusk test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit('http://localhost:8000')
+                ->assertTitle('Laravel')
+                ->clickLink('Login')
+                ->type('#email', 'ravali@gmail.com')
+                ->type('#password', 'Ravali@123')
+                ->press('button[type="submit"]')
+                ->assertSee('Questions')
+                ->clickLink('Create a Question')
+                ->assertSee('Create Question')
+                ->type('#body', 'what is your name?')
+                ->press('#submit')
+                ->assertSee('IT WORKS!')
+                ->clickLink('View')
+                ->assertSee('Question')
+                ->clickLink('Edit Question')
+                ->assertSee('Body')
+                ->type('#body', 'what is your birth place?')
+                ->click('#submit')
+                ->assertSee('Saved')
+                ->press('#navbarDropdown')
+                ->clickLink('Logout')
+                ->assertTitle('Laravel');
+        });
+    }
+}
